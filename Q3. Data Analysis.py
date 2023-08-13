69965c298cbd748b1e4c23df363161851e586230
B14ck0ut <gavinhe00@gmail.com>
2023-08-12 23:18:51 -0400

    First commit

---
+++ Q3. Data Analysis.py
@@ -0,0 +1,40 @@
+import json
+import pandas as pd
+import numpy as np
+
+def print_full(x):
+    """ Disable the print limit of data frame and print out the given data frame
+
+    :param x: data frame to be fully printed
+    """
+    pd.set_option('display.max_rows', len(x))
+    pd.set_option('display.max_columns', len(x.index))
+    print(x)
+    pd.reset_option('display.max_rows')
+    pd.reset_option('display.max_columns')
+
+files = ["receipts.json", "users.json", "brands.json"]
+for file in files:
+    print(file)
+    file = "json files/" + file
+    # Read file
+    with open(file, "r", encoding="utf-8") as f:
+        data_list = []
+        for line in f:
+            data = json.loads(line)
+            data_list.append(data)
+        df = pd.json_normalize(data_list)
+    print(df.dtypes, "\n")
+    # Check for missing values
+    missing_values_count = df.isnull().sum()
+    total_cells = np.product(df.shape)
+    total_missing = missing_values_count.sum()
+    percent_missing = (total_missing / total_cells) * 100
+    if percent_missing > 0:
+        print("Missing data found:")
+        print(missing_values_count[missing_values_count > 0])
+    print("Data missing rate: {:.3f}%\n".format(percent_missing))
+
+
+
+
